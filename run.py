e8e35eb2353e33fbc696601d47951b4489030238
Moringa School <loisemburu01@gmail.com>
2019-12-16 17:07:40 +0300

    changed the else statement

--- run.py
+++ run.py
@@ -96,7 +96,7 @@ def main():
             print("exit from password locker")
             break
         else:
-                print("I really didn't get that. Please use the short codes")
+                print("Please use the short codes")
 
 if __name__ == '__main__':
     main()
