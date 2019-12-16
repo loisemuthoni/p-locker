89d9696342687255c21e952d3a11889363545032
Moringa School <loisemburu01@gmail.com>
2019-12-16 07:48:50 +0300

    added class variables

---
+++ user.py
@@ -0,0 +1,11 @@
+class user:
+    user_list = []
+
+    def __init__(self,account_name,last_name,phone_number,email):
+
+         
+          
+        self.account_name = account_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
